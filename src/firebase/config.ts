bae9e70b807de510072fe8d418ecb09b59a7edb3
Stephen Oloo <stephen@hellotractor.com>
2025-12-28 12:20:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "account-management-dashb-51e96",
+  "appId": "1:855822166646:web:28560c4bdd9dd81eda0d57",
+  "apiKey": "AIzaSyADQH_o6dONmC5YGXSvol5JWRSrBsMzg9w",
+  "authDomain": "account-management-dashb-51e96.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "855822166646"
+};
